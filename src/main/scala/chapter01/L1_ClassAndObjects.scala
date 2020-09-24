7cf9442bfe2107bd98ab50485b921c87c334432c
lei-hsia <GenuineLei@163.com>
2020-09-24 15:43:23 +0800

    class和object同名的伴生类和伴生对象

--- src/main/scala/chapter01/L1_ClassAndObjects.scala
+++ src/main/scala/chapter01/L1_ClassAndObjects.scala
@@ -1,5 +1,12 @@
 package chapter01
 
+class L1_ClassAndObjects() {
+    // 同一个文件中,class和object名称相同时, 这个class叫做object的伴生类,object叫做class的伴生对象, 相互可以访问对方的私有变量
+    private val index = 5
+}
+
+//在类当中重写构造，构造中第一行需要调用默认构造
+//在Scala当中，new Person时 出来方法不执行【除了构造方法】其他都执行
 class Person(uname:String, uage:Int) {
     val name = uname
     val age = uage
@@ -30,6 +37,10 @@ object L1_ClassAndObjects {
         val person = new Person("lei", 24) // new xxx()生成对象时，除了方法不执行，其他的都执行
         println(person.name)
         println(person.age)
+        println(person.gender)
+
+        val obj = new L1_ClassAndObjects()
+        println(obj.index)
         println(person.gender)
     }
 }
