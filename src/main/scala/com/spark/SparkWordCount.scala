112087d7d2ab4a02c4b08d13367482dbbcdd908b
lei-hsia <GenuineLei@163.com>
2020-09-30 16:23:44 +0800

    one-line spark wordcount demo

--- src/main/scala/com/spark/SparkWordCount.scala
+++ src/main/scala/com/spark/SparkWordCount.scala
@@ -12,7 +12,7 @@ object SparkWordCount {
     conf.set("spark.driver.memory", "1"); // 运行资源
     // SparkContext是通往spark集群的唯一通道
     val sc = new SparkContext(conf)
-
+    /*
     val lines: RDD[String] = sc.textFile("./data/words")
     val words: RDD[String] = lines.flatMap(line => {
       line.split(" ")
@@ -23,6 +23,8 @@ object SparkWordCount {
     val result: RDD[(String, Int)] = pairWords.reduceByKey((v1: Int, v2: Int) => {
       v1 + v2
     })
-    result.foreach(one=>println(one))
+    result.foreach(one=>println(one))*/
+
+    sc.textFile("./data/words").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).foreach(println)
   }
 }
